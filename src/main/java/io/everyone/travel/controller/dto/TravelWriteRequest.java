ce7d9476cf0c8b567f7c3abfc3232f0abc876d96
deneb1593 <deneb1593@gmail.com>
2023-11-10 21:36:49 +0900

    Swagger 설명 추가

--- src/main/java/io/everyone/travel/controller/dto/TravelWriteRequest.java
+++ src/main/java/io/everyone/travel/controller/dto/TravelWriteRequest.java
@@ -1,5 +1,6 @@
 package io.everyone.travel.controller.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
@@ -9,22 +10,28 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Schema(description = "여행 정보 작성")
 @Value
 @Builder
 public class TravelWriteRequest {
 
+    @Schema(description = "여행 시작일", example = "2023-11-10T12:27:21.042Z")
     @NotNull(message = "여행 시작일은 필수 값입니다")
     LocalDateTime startAt;
 
+    @Schema(description = "여행 종료일", example = "2023-11-17T10:00:00.00Z")
     @NotNull(message = "여행 종료일은 필수 값입니다")
     LocalDateTime endAt;
 
+    @Schema(description = "여행명", example = "태국 여행")
     @NotBlank(message = "여행 명은 필수 값입니다")
     String title;
 
-    List<PlanWriteRequest> plans; // 계획 목록
+    @Schema(description = "계획 목록")
+    List<PlanWriteRequest> plans;
 
-    List<ExpenseWriteRequest> expenses; // 지출 목록
+    @Schema(description = "지출 목록")
+    List<ExpenseWriteRequest> expenses;
 
     @Value
     @Builder
