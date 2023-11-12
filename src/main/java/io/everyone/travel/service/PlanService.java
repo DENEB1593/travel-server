df0deed48548c171b16b83e808e84433d52025f1
deneb1593 <deneb1593@gmail.com>
2023-11-12 15:05:12 +0900

    코드 정리

--- src/main/java/io/everyone/travel/service/PlanService.java
+++ src/main/java/io/everyone/travel/service/PlanService.java
@@ -2,14 +2,12 @@ package io.everyone.travel.service;
 
 import io.everyone.travel.domain.Plan;
 import io.everyone.travel.domain.Travel;
-import io.everyone.travel.repository.PlanRepository;
 import io.everyone.travel.repository.TravelRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
