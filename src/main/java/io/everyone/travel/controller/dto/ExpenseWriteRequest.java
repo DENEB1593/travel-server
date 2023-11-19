6d01db9bfec9a724d7db1ccdb6670e7449d710bb
deneb1593 <deneb1593@gmail.com>
2023-11-19 21:41:38 +0900

    record 변경으로 인한 롬복 설정 삭제

--- src/main/java/io/everyone/travel/controller/dto/ExpenseWriteRequest.java
+++ src/main/java/io/everyone/travel/controller/dto/ExpenseWriteRequest.java
@@ -7,7 +7,6 @@ import lombok.Value;
 import java.math.BigDecimal;
 
 @Schema(name = "지출 작성 요청", description = "지출 작성을 요청한다")
-@Value
 public record ExpenseWriteRequest(
     @Schema(description = "지출 비용", example = "4500")
     @NotNull(message = "지출 비용은 필수 값입니다")
