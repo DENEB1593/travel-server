8a137b2c45ea1b128daa9d173cec37fe3f103416
deneb1593 <deneb1593@gmail.com>
2023-11-25 13:37:23 +0900

    [#12] security swagger ignore 설정

--- src/main/java/io/everyone/travel/security/SecurityConfig.java
+++ src/main/java/io/everyone/travel/security/SecurityConfig.java
@@ -1,29 +1,43 @@
 package io.everyone.travel.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable);
-        // TODO oAuth Test 추가
-
-        return http.build();
+        return http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth ->
+                auth
+                    .requestMatchers(antMatcher("/login")).permitAll()
+                    .requestMatchers(antMatcher("/api-docs/**")).permitAll()
+                    .requestMatchers(antMatcher("/swagger-ui/**")).permitAll()
+                    .requestMatchers(antMatcher("/**")).permitAll() // 개발 중일 때는 permit all
+            )
+            .build();
     }
 
     @Bean
     WebSecurityCustomizer webSecurityCustomizer() {
         return web -> web.ignoring()
-            .requestMatchers("/api-docs")
+            .requestMatchers("/api-docs/**")
             .requestMatchers("/swagger-ui/**");
     }
 
