e7f3bfe7dc67d049761566066822ca49dfd24cd5
deneb1593 <deneb1593@gmail.com>
2023-11-30 10:19:16 +0900

    [#12] 사용자 정보 NOT FOUND 예외 추가

--- src/main/java/io/everyone/travel/security/oauth/jwt/JwtAuthenticationFilter.java
+++ src/main/java/io/everyone/travel/security/oauth/jwt/JwtAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package io.everyone.travel.security.oauth.jwt;
 
 import io.everyone.travel.domain.User;
+import io.everyone.travel.exception.NotFoundException;
 import io.everyone.travel.repository.UserRepository;
 import io.everyone.travel.security.oauth.OAuth2TravelUser;
 import jakarta.servlet.FilterChain;
@@ -13,7 +14,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -22,7 +22,6 @@ import javax.annotation.Nonnull;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @Component
@@ -52,16 +51,20 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         if (StringUtils.hasText(email) || context.getAuthentication() == null) {
             // 토큰 유효성 검사( 만료시간, 사용자 존재 등)
-            userRepository.findByEmail(email);
 
-            OAuth2User oAuth2User = OAuth2TravelUser.builder()
+            User user = userRepository
+                .findByEmail(email)
+                .orElseThrow(NotFoundException::forUser);
+
+            var oAuth2User = OAuth2TravelUser.builder()
                 .attributes(new HashMap<>())
                 .authorities(List.of(new SimpleGrantedAuthority("USER")))
                 .email(email)
                 .build();
 
             var authentication
-                = new UsernamePasswordAuthenticationToken(oAuth2User, null, null);
+                = new UsernamePasswordAuthenticationToken(oAuth2User, null, oAuth2User.getAuthorities());
+
             context.setAuthentication(authentication);
             SecurityContextHolder.setContext(context);
         }
