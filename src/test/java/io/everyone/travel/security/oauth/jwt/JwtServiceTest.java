f139d6473893d5bf67ac894d8afb7627103cf425
deneb1593 <deneb1593@gmail.com>
2023-12-02 14:35:06 +0900

    [#12] access token 테스트 추가

--- src/test/java/io/everyone/travel/security/oauth/jwt/JwtServiceTest.java
+++ src/test/java/io/everyone/travel/security/oauth/jwt/JwtServiceTest.java
@@ -1,13 +1,16 @@
 package io.everyone.travel.security.oauth.jwt;
 
+import io.everyone.travel.security.jwt.JwtProperties;
+import io.everyone.travel.security.jwt.JwtService;
 import io.everyone.travel.security.oauth.OAuth2UserDetails;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-//@Disabled
+@Disabled
 @SpringBootTest
 class JwtServiceTest {
 
@@ -42,13 +45,24 @@ class JwtServiceTest {
             .build();
 
         String token = jwtService.generateToken(mock);
-
         assertThat(token).isNotNull();
 
         var claim = jwtService.extractToken(token);
-        String subject = claim.getSubject();
-        System.out.println(subject);
+        assertThat(claim).isNotNull();
+        assertThat(claim.getSubject()).isEqualTo(mock.getEmail());
+    }
 
+    @Test
+    void tokenValidTests() {
+        // naver sample
+        String givenToken = "eyJhbGciOiJIUzM4NCJ9.eyJpc3MiOiJ0cmF2ZWwtc2VydmVyLWF1dGgiLCJzdWIiOiJsZWVrYW5nd29vbjFAbmF2ZXIuY29tIiwiaWF0IjoxNzAxNDk0NzA4LCJleHAiOjE3MDE0OTgzMDh9._FdTCDD3R6vR1WYt0vp5xmhNmd5-M2ruWLyQ62CnlnrPSPmFTxotalZxZRgBPQKk";
+        String originEmail = "leekangwoon1@naver.com";
+        assertThat(jwtService.isTokenValid(givenToken, originEmail)).isTrue();
+
+        // kakao sample
+        String givenToken2 = "eyJhbGciOiJIUzM4NCJ9.eyJpc3MiOiJ0cmF2ZWwtc2VydmVyLWF1dGgiLCJzdWIiOiJsZWVrYW5nd29vbkBuYXRlLmNvbSIsImlhdCI6MTcwMTQ5NTAxNCwiZXhwIjoxNzAxNDk4NjE0fQ.s2J4s35XVZdCMxXgqZcFzJLLFEeiTgSLRtG9q17opj_vTbzUo_JWsQT4FBmVoeoq";
+        String originEmail2 = "leekangwoon@nate.com";
+        assertThat(jwtService.isTokenValid(givenToken2, originEmail2)).isTrue();
     }
 
 
