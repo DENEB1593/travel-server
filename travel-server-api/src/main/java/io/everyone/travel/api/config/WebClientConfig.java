facec6abb5c42527922055e09105423d4f5ed2ec
deneb1593 <deneb1593@gmail.com>
2024-02-29 01:22:18 +0900

    add: WebClient 추가

--- travel-server-api/src/main/java/io/everyone/travel/api/config/WebClientConfig.java
+++ travel-server-api/src/main/java/io/everyone/travel/api/config/WebClientConfig.java
@@ -1,12 +1,59 @@
 package io.everyone.travel.api.config;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorResourceFactory;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
 
-    public void publicDataClient() {
+    // TODO API Properties
 
+    @Bean("publicDataClient")
+    public WebClient publicDataClient(
+        ReactorResourceFactory reactorResourceFactory
+    ) {
+        return WebClient.builder()
+            .baseUrl("/public/data")
+            .defaultHeaders(httpHeaders -> {
+                httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+            })
+            .clientConnector(
+                this.customizeConnector(reactorResourceFactory)
+            )
+            .build();
+    }
+
+    private ClientHttpConnector customizeConnector(
+        ReactorResourceFactory reactorResourceFactory
+    ) {
+        return new ReactorClientHttpConnector(reactorResourceFactory, httpClient -> {
+            ConnectionProvider provider = ConnectionProvider.builder("public-data")
+                .maxConnections(100)
+                .pendingAcquireMaxCount(1000)
+                .maxIdleTime(Duration.ofMillis(1000))
+                .maxLifeTime(Duration.ofMillis(1000))
+                .build();
+
+            return HttpClient.create(provider)
+                .metrics(true, uri -> "/")
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 1000)
+                .doOnConnected(connection -> {
+                    connection.addHandlerLast(
+                        new ReadTimeoutHandler(1000)
+                    );
+                });
+        });
     }
 
 }
